a24480c973e9d20191b555504db0f9fa4d3df289
zato91 <zatokey@gmail.com>
2020-07-06 17:54:48 -0400

    testing databse

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
      has_many :companies, through: :user_companies
      has_many :posts
      has_many :user_skills
-     has_many :skills, through :user_skills
+     has_many :skills, through: :user_skills
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Company.destroy_all
+JobListing.destroy_all
+JobSkill.destroy_all
+Post.destroy_all
+Skill.destroy_all
+UserCompany.destroy_all
+UserSkill.destroy_all
+
+
+luis = User.create(name: "Luis", email:"123@gmail.com", password_digest:"123")
+apple = Company.create(name:"Apple", location:"Califorina", description:"makes simple use products",industry:"Tech")
+usercompany1 = UserCompany.create(user_id:luis.id,company_id: apple.id)
+
+
+post1 = Post.create(title:"promotion", message:"Super happy new position",user: luis)
+job1 = JobListing.create(title:"web developper", description:"code things",salary:"100k",company: apple)
+skill1 = Skill.create(name:"Ruby")
+skill2 = Skill.create(name:"Project managemnt")
+skill3 = Skill.create(name:"Design")
+
+
+
