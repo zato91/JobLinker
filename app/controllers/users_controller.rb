a639bd65ec23a55d81e3dbad57887b833dc869b0
zato91 <zatokey@gmail.com>
2020-07-08 14:17:27 -0400

    add picture feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
     private 
     
     def user_params
-        params.require(:user).permit(:name, :email, :password)
+        params.require(:user).permit(:name, :email, :password, :image)
     end 
 
     def find_user
