2e04838787dd22b62a9f9f528a6c7879bbcb9f51
lf261 <luis.fernandez2093@gmail.com>
2020-07-08 15:53:54 -0400

    test profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
     private 
     
     def user_params
-        params.require(:user).permit(:name, :email, :password, :image)
+        params.require(:user).permit(:name, :email, :password, :image, :company, :skill => [])
     end 
 
     def find_user
