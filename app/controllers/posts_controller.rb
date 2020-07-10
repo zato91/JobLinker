dd734dc32ad4f3e5e384e5814279ab2cddf0ac80
lf261 <luis.fernandez2093@gmail.com>
2020-07-10 10:42:25 -0400

    merging code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,17 @@
 class PostsController < ApplicationController
  
+    def new 
+        @post = Post.new
+    end 
+
+    def create
+        @post = Post.create(post_params)
+        redirect_to post_path(@post)
+    end 
+
+    private 
+    def post_params
+        params.require(:post).permit(:title, :message, :user_ids => [])
+    end 
+
 end
